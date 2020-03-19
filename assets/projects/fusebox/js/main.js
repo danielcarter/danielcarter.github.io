a9d2c8a76542a7efdbdc334964bd46211a49803a
Daniel Carter <dcarter@txstate.edu>
2020-03-19 12:59:54 -0500

    add fusebox site

---
+++ assets/projects/fusebox/js/main.js
@@ -0,0 +1,10 @@
+$( document ).ready(function() {
+
+  var jqxhr = $.getJSON( "https://docs.google.com/spreadsheets/d/e/2PACX-1vQuQfdKizqCzrFnvDpaqga5Hry5EM3B44Nv7-24D-w8nReon2O9T1DkKJu-76NsA0f64X6E6KLXqEvH/pub?output=json", function() {
+    console.log( "success" );
+  })
+
+
+
+
+});
